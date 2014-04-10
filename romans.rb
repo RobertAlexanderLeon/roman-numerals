413c1d81c8e748dea1f26b02450893fbd338c1e4
Robert Leon <llexi@hotmail.com>
2014-04-10 16:43:37 +0100

    Stated challenge notes and added Hash of Roman Numberal values

---
+++ romans.rb
@@ -0,0 +1,20 @@
+
+# Roman Numerals
+
+# "Write a program that converts arabic numbers into roman numerals. 
+# It would convert the arabic number 1 into the roman numeral I, 
+# the arabic 1999 into the roman numeral MCMXCIX and so forth."
+
+ROMAN_MAP = {     1 => "I",
+                  4 => "IV",
+                  5 => "V",
+                  9 => "IX",
+                  10 => "X",
+                  40 => "XL",
+                  50 => "L",
+                  90 => "XC",
+                  100 => "C",
+                  400 => "CD",
+                  500 => "D",
+                  900 => "CM",
+                  1000 => "M" }
